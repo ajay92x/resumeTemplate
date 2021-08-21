ec3346db5aba631231cba10ac6227c48f03695a2
abhi-wat101 <abhi.wat101@gmail.com>
2021-08-21 20:44:59 +0530

    check3

---
+++ cv3/js/custom.js
@@ -0,0 +1,227 @@
+let projectJSON = [
+    {
+        "href": "https://citynative.matrixmarketers.com/",
+        "title": "City Native",
+        "type": "Website",
+        "img": "img/portfolio/citynative.png",
+        "isLink": true
+    },
+    {
+        "href": "https://dwellable.matrixmarketers.com/",
+        "title": "Dwellable",
+        "type": "Website",
+        "img": "img/portfolio/dwellable.png",
+        "isLink": true
+    },{
+    "href": "https://homejab.com/",
+    "title": "Homejab",
+    "type": "Website",
+    "img": "img/portfolio/homejab.png",
+    "isLink": true
+}, {
+    "href": "https://citrus.matrixmarketers.com/",
+    "title": "CitrusCow",
+    "type": "Website",
+    "img": "img/portfolio/citruscow.png",
+    "isLink": true
+}, {
+    "href": "img/portfolio/monero.png",
+    "title": "Monero",
+    "type": "Website",
+    "img": "img/portfolio/monero.png",
+    "isLink": false
+}, {
+    "href": "img/portfolio/pageiq.jpg",
+    "title": "PageIQ",
+    "type": "Website",
+    "img": "img/portfolio/pageiq.jpg",
+    "isLink": false
+}, {
+    "href": "img/portfolio/urbankube.png",
+    "title": "UrbanKube",
+    "type": "Website",
+    "img": "img/portfolio/urbankube.png",
+    "isLink": false
+}, {
+    "href": "img/portfolio/tvs.png",
+    "title": "Malhotra TVS",
+    "type": "Mobile Application",
+    "img": "img/portfolio/tvs.png",
+    "isLink": false
+}, {
+    "href": "img/portfolio/dps.png",
+    "title": "Delhi Public School, Chhindwara",
+    "type": "Mobile Application",
+    "img": "img/portfolio/dps.png",
+    "isLink": false
+}, {
+    "href": "https://m.allfreeapk.com/shopping-zone,2671426/",
+    "title": "Shopping Zone",
+    "type": "Mobile Application",
+    "img": "img/portfolio/shopping_zone.png",
+    "isLink": true
+}, {
+    "href": "https://m.allfreeapk.com/re-assist,12216879/",
+    "title": "Royal Enfield",
+    "type": "Mobile Application",
+    "img": "img/portfolio/royal-enfield.png",
+    "isLink": true
+}, {
+    "href": "https://play.google.com/store/apps/details?id=com.carsake.innovativehonda",
+    "title": "Innovative Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": true
+}, {
+    "href": "https://m.allfreeapk.com/bhagwati-honda,1976463/",
+    "title": "Bhagwati Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": true
+}, {
+    "href": "https://m.allfreeapk.com/om-honda,12279586/",
+    "title": "Om Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": true
+}, {
+    "href": "https://m.allfreeapk.com/sgbl-honda,11914063/",
+    "title": "SGBL Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": true
+}, {
+    "href": "https://m.allfreeapk.com/kamdhenu-honda,11963023/",
+    "title": "Kamdhenu Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": true
+}, {
+    "href": "img/portfolio/honda.png",
+    "title": "Akshit Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": false
+}, {
+    "href": "img/portfolio/honda.png",
+    "title": "Platinum Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": false
+}, {
+    "href": "img/portfolio/honda.png",
+    "title": "Sai Point Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": false
+}, {
+    "href": "img/portfolio/honda.png",
+    "title": "Gandhi Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": false
+}, {
+    "href": "img/portfolio/honda.png",
+    "title": "Sarvpriya Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": false
+}, {
+    "href": "img/portfolio/honda.png",
+    "title": "Axis Honda",
+    "type": "Mobile Application",
+    "img": "img/portfolio/honda.png",
+    "isLink": false
+}, {
+    "href": "https://play.google.com/store/apps/details?id=com.carsake.app.manoryamha",
+    "title": "Manor Yamaha",
+    "type": "Mobile Application",
+    "img": "img/portfolio/yamaha.jpg",
+    "isLink": true
+}, {
+    "href": "img/portfolio/yamaha.jpg",
+    "title": "Carlton Yamaha",
+    "type": "Mobile Application",
+    "img": "img/portfolio/yamaha.jpg",
+    "isLink": false
+}, {
+    "href": "img/portfolio/yamaha.jpg",
+    "title": "SNC Yamaha",
+    "type": "Mobile Application",
+    "img": "img/portfolio/yamaha.jpg",
+    "isLink": false
+}, {
+    "href": "img/portfolio/yamaha.jpg",
+    "title": "Bikerz Yamaha",
+    "type": "Mobile Application",
+    "img": "img/portfolio/yamaha.jpg",
+    "isLink": false
+}, {
+    "href": "http://tagzie.com/",
+    "title": "Tagzie",
+    "type": "Mobile Application",
+    "img": "img/portfolio/tagzie.jpeg",
+    "isLink": true
+}, {
+    "href": "https://play.google.com/store/apps/details?id=com.intervuelive&hl=en_IN",
+    "title": "IntervueLive",
+    "type": "Mobile Application",
+    "img": "img/portfolio/intervuelive.png",
+    "isLink": true
+}, {
+    "href": "https://play.google.com/store/apps/details?id=com.talktosanta&hl=en_IN",
+    "title": "Talk to Santa",
+    "type": "Mobile Application",
+    "img": "img/portfolio/talktosanta.png",
+    "isLink": true
+}, {
+    "href": "img/portfolio/caterpillar.png",
+    "title": "Caterpillar",
+    "type": "Mobile Application",
+    "img": "img/portfolio/caterpillar.png",
+    "isLink": false
+}, {
+    "href": "https://play.google.com/store/apps/details?id=com.defeatmsa.brainsupporthub&hl=en_IN",
+    "title": "GOSH:Global Online Support Hub",
+    "type": "Mobile Application",
+    "img": "img/portfolio/gosh.png",
+    "isLink": true
+}, {
+    "href": "img/portfolio/kliggit.jpg",
+    "title": "Kliggit",
+    "type": "Mobile Application",
+    "img": "img/portfolio/kliggit.jpg",
+    "isLink": false
+}, {
+    "href": "img/portfolio/birdieball.webp",
+    "title": "Birdieball",
+    "type": "Mobile Application",
+    "img": "img/portfolio/birdieball.webp",
+    "isLink": false
+}]
+
+$(document).ready(() => {
+    $('.work').empty();
+
+    projectJSON.forEach((item) => {
+
+        let attribute = item.isLink ? 'target="_blank"' : 'class="popup"';
+        let elementType = item.type == 'Website' ? 'website' : 'mobile-app';
+
+        let element = `<li class="col-sm-4">
+        <div class="item ${elementType}">
+            <a href="${item.href}" ${attribute}>
+                <div class="desc">
+                    <h5 class="proj-desc">
+                        ${item.title}
+                        <span>${item.type}</span>
+                    </h5>
+                </div>
+                <img src="${item.img}" alt="">
+            </a>
+        </div>
+    </li>`;
+
+        $('.work').append(element);
+    })
+})
